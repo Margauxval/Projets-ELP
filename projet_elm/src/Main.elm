d88d071e24c641ac76cf27b222204d5679c9f8a6
lilound <lilounadlerr@gmail.com>
2026-01-14 11:41:09 +0100

    Update Main.elm
    ajout de svg avec formes en commentaires

--- projet_elm/src/Main.elm
+++ projet_elm/src/Main.elm
@@ -4,7 +4,31 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
+{- main =
+  svg
+    [ width "120"
+    , height "120"
+    , viewBox "0 0 120 120"
+    ]
+    [ rect
+        [ x "10"
+        , y "10"
+        , width "100"
+        , height "100"
+        , rx "15"
+        , ry "15"
+        ]
+        []
+    , circle
+        [ cx "50"
+        , cy "50"
+        , r "50"
+        ]
+        []
+    ] -}
 
 -- MAIN
 
