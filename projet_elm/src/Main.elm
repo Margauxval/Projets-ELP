cb4f4ba0b35d27aa7a7892008e5b4c09a1768163
Margauxval <margauxvallee09@icloud.com>
2026-01-16 11:22:21 +0100

    Refactor Main.elm for improved functionality

--- projet_elm/src/Main.elm
+++ projet_elm/src/Main.elm
@@ -1,113 +1,236 @@
-
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (..)
+import Html exposing (Html, button, div, h2, textarea, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
-
-{- main =
-  svg
-    [ width "120"
-    , height "120"
-    , viewBox "0 0 120 120"
-    ]
-    [ rect
-        [ x "10"
-        , y "10"
-        , width "100"
-        , height "100"
-        , rx "15"
-        , ry "15"
-        ]
-        []
-    , circle
-        [ cx "50"
-        , cy "50"
-        , r "50"
-        ]
-        []
-    ] -}
-
--- MAIN
-
-main =
-    Browser.sandbox { init = init, update = update, view = view }
+import Html.Events exposing (onClick, onInput)
+import ParserTcTurtle exposing (Instruction(..), read)
+import Drawing exposing (display)
 
 
 -- MODEL
 
 type alias Model =
-    Int
+    { source : String
+    , result : Result String (List Instruction)
+    , color : String
+    }
 
 
 init : Model
 init =
-    0
+    { source = "[ ]"
+    , result = Ok []
+    , color = "black"
+    }
+
+
+-- MESSAGES
+
+type Msg
+    = UpdateSource String
+    | AddForward
+    | AddLeft
+    | AddRight
+    | AddSquare
+    | AddTriangle
+    | AddCircle
+    | AddTurtle
+    | ClearProgram
+    | Undo
+    | SetColor String
 
 
 -- UPDATE
 
-type Msg
-    = Couleur
-    | Avancer
-    | Tourner
-    | Cercle
-    | Triangle
-    | Tortue
-    | Rectangle
-
-
 update : Msg -> Model -> Model
 update msg model =
+    let
+        autoRun newModel =
+            { newModel | result = read newModel.source }
+    in
     case msg of
-        Couleur ->
-            model + 1
+        UpdateSource txt ->
+            autoRun { model | source = txt }
 
-        Avancer ->
-            model + 1
+        AddForward ->
+            autoRun (appendInstruction "Forward 20" model)
 
-        Tourner ->
-            model + 1
+        AddLeft ->
+            autoRun (appendInstruction "Left 15" model)
 
-        Cercle ->
-            model + 1
+        AddRight ->
+            autoRun (appendInstruction "Right 15" model)
 
-        Triangle ->
-            model + 1
+        AddSquare ->
+            autoRun (appendInstruction "Repeat 4 [Forward 80, Left 90]" model)
 
-        Tortue ->
-            model + 1
+        AddTriangle ->
+            autoRun (appendInstruction "Repeat 3 [Forward 100, Left 120]" model)
 
-        Rectangle ->
-            model + 1
+        AddCircle ->
+            autoRun (appendInstruction "Repeat 360 [Forward 2, Left 1]" model)
+
+        AddTurtle ->
+            autoRun
+                (appendInstruction
+                    """
+                    Repeat 1 [
+                        Repeat 6 [Forward 60, Left 60],
+                        Left 90, Forward 20, Left 180, Forward 20, Left 180,
+                        Right 150, Forward 20, Left 180, Forward 20, Left 180,
+                        Left 60, Forward 20, Left 180, Forward 20, Left 180,
+                        Right 150, Forward 20, Left 180, Forward 20, Left 180,
+                        Left 90, Forward 10, Left 180, Forward 10, Left 180
+                    ]
+                    """
+                    model
+                )
+                
+        ClearProgram ->
+            autoRun { model | source = "[ ]" }
+
+        Undo ->
+            autoRun (undoInstruction model)
+
+        SetColor col ->
+            autoRun { model | color = col }
+
+
+appendInstruction : String -> Model -> Model
+appendInstruction instr model =
+    let
+        newSource =
+            case model.source of
+                "[ ]" ->
+                    "[ " ++ instr ++ " ]"
+
+                _ ->
+                    let
+                        inside =
+                            String.dropLeft 1 (String.dropRight 1 model.source)
+                    in
+                    "[ " ++ inside ++ ", " ++ instr ++ " ]"
+    in
+    { model | source = newSource }
+
+
+undoInstruction : Model -> Model
+undoInstruction model =
+    case model.source of
+        "[ ]" ->
+            model
+
+        _ ->
+            let
+                inside =
+                    String.dropLeft 1 (String.dropRight 1 model.source)
+
+                items =
+                    String.split "," inside
+                        |> List.map String.trim
+
+                newItems =
+                    case List.reverse items of
+                        [] ->
+                            []
+
+                        _ :: rest ->
+                            List.reverse rest
+
+                newSource =
+                    case newItems of
+                        [] ->
+                            "[ ]"
+
+                        _ ->
+                            "[ " ++ String.join ", " newItems ++ " ]"
+            in
+            { model | source = newSource }
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Couleur ] [ Html.text "Changer de couleur" ]
-        , div [] [ Html.text (String.fromInt model) ]
-
-        , button [ onClick Avancer ] [ Html.text "Avancer" ]
-        , div [] [ Html.text (String.fromInt model) ]
-
-        , button [ onClick Tourner ] [ Html.text "Tourner" ]
-        , div [] [ Html.text (String.fromInt model) ]
-
-        , button [ onClick Cercle ] [ Html.text "Cercle" ]
-        , div [] [ Html.text (String.fromInt model) ]
-
-        , button [ onClick Triangle ] [ Html.text "Triangle" ]
-        , div [] [ Html.text (String.fromInt model) ]
-
-        , button [ onClick Tortue ] [ Html.text "Tortue" ]
-        , div [] [ Html.text (String.fromInt model) ]
-
-        , button [ onClick Rectangle ] [ Html.text "Rectangle" ]
-        , div [] [ Html.text (String.fromInt model) ]
+    div
+        [ style "padding" "20px"
+        , style "font-family" "'Quicksand', sans-serif"
+        , style "background-color" "#d8ecff"
+        , style "min-height" "100vh"
         ]
+        [ h2 [] [ text "TcTurtle – Interface améliorée" ]
+
+        , div
+            [ style "display" "flex"
+            , style "gap" "40px"
+            , style "align-items" "flex-start"
+            ]
+            [
+              -- Colonne gauche : boutons
+              div []
+                [ h2 [] [ text "Mouvement" ]
+                , button [ onClick AddForward ] [ text "Forward 20" ]
+                , button [ onClick AddLeft, style "margin-left" "10px" ] [ text "Left 15°" ]
+                , button [ onClick AddRight, style "margin-left" "10px" ] [ text "Right 15°" ]
+
+                , div [ style "margin" "20px 0" ]
+                    [ h2 [] [ text "Formes" ]
+                    , button [ onClick AddSquare ] [ text "Carré" ]
+                    , button [ onClick AddTriangle, style "margin-left" "10px" ] [ text "Triangle" ]
+                    , button [ onClick AddCircle, style "margin-left" "10px" ] [ text "Cercle" ]
+                    , button [ onClick AddTurtle, style "margin-left" "10px" ] [ text "Tortue" ]
+                    ]
+
+                , div [ style "margin" "20px 0" ]
+                    [ h2 [] [ text "Couleurs" ]
+                    , button [ onClick (SetColor "black") ] [ text "Noir" ]
+                    , button [ onClick (SetColor "red"), style "margin-left" "10px" ] [ text "Rouge" ]
+                    , button [ onClick (SetColor "blue"), style "margin-left" "10px" ] [ text "Bleu" ]
+                    , button [ onClick (SetColor "green"), style "margin-left" "10px" ] [ text "Vert" ]
+                    ]
+
+                , div [ style "margin" "20px 0" ]
+                    [ h2 [] [ text "Contrôle" ]
+                    , button [ onClick Undo ] [ text "Undo" ]
+                    , button [ onClick ClearProgram, style "margin-left" "10px" ] [ text "Clear" ]
+                    ]
+                ]
+
+              -- Colonne droite : zone de texte + dessin
+            , div []
+                [ textarea
+                    [ value model.source
+                    , onInput UpdateSource
+                    , rows 10
+                    , cols 50
+                    , style "font-size" "16px"
+                    ]
+                    []
+
+                , div
+                    [ style "margin-top" "20px"
+                    , style "border" "1px solid #ccc"
+                    , style "padding" "10px"
+                    , style "display" "inline-block"
+                    , style "background" "white"
+                    ]
+                    [ viewResult model ]
+                ]
+            ]
+        ]
+
+
+viewResult : Model -> Html msg
+viewResult model =
+    case model.result of
+        Err msg ->
+            div [ style "color" "red" ] [ text msg ]
+
+        Ok program ->
+            display model.color program
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
